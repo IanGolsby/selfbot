74747d9c3060ecf5fd7bc4a7b37b7a38e3a01931
Ian Golsby <iangolsby49@gmail.com>
2018-01-14 22:36:26 -0600

    Nightly save

--- selfbot.py
+++ selfbot.py
@@ -1,7 +1,7 @@
 import discord
-import asyncio
+from discord.ext import commands
 
-token = ""
+token = "MjI0NzEyMDYzODMyOTQ4NzM2.Cree6w.fjA5WKPVuHhXRGAzG5rOmS3CseE"
 client = discord.Client()
 
 @client.event
@@ -13,10 +13,17 @@ async def on_ready():
 
     await client.change_presence(game=discord.Game(name="Discord.py"))
 
+    idgo = client.user
+
 @client.event
 async def on_message(message):
     if message.author.id == '224712063832948736':
+        # General Commands
         if message.content.startswith('>help'):
             await client.send_message(message.channel, '**Commands List:**\n`>help` - sends this message. Lists all commands.')
-
+        # Moderation Commands
+        if message.content.startswith('>purge'):
+            if idgo.permissions_in(message.channel).manage_messages:
+                
+                
 client.run(token, bot=False)
