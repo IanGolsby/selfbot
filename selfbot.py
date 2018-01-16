8360b648ec0c647054a472930aea9806ff501164
Ian Golsby <iangolsby49@gmail.com>
2018-01-15 21:29:11 -0600

    Removed token and finished purge

--- selfbot.py
+++ selfbot.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 
-token = "MjI0NzEyMDYzODMyOTQ4NzM2.Cree6w.fjA5WKPVuHhXRGAzG5rOmS3CseE"
+token = "I got hacked so I'm changing this the fuck up and now new token and not putting that on github again cause I was tired last night when I pushed the update"
 client = discord.Client()
 
 @client.event
@@ -11,19 +11,18 @@ async def on_ready():
     print(client.user.id)
     print("-----")
 
-    await client.change_presence(game=discord.Game(name="Discord.py"))
-
-    idgo = client.user
+    #await client.change_presence(game=discord.Game(name="Discord.py"))
 
 @client.event
 async def on_message(message):
-    if message.author.id == '224712063832948736':
+    if message.author.id == client.user.id:
         # General Commands
         if message.content.startswith('>help'):
-            await client.send_message(message.channel, '**Commands List:**\n`>help` - sends this message. Lists all commands.')
+            await client.send_message(message.channel, '**Commands List:**\n`>help` - sends this message. Lists all commands.\n`>purge [x]` - deletes x messages from the channel.')
         # Moderation Commands
         if message.content.startswith('>purge'):
-            if idgo.permissions_in(message.channel).manage_messages:
-                
-                
+            if client.user.permissions_in(message.channel).manage_messages and client.user.permissions_in(message.channel).read_message_history:
+                async for message in client.logs_from(message.channel, limit=int(message.content[7:])+1):
+                    await client.delete_message(message)
+        
 client.run(token, bot=False)
